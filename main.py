6223d519aaee6d4fcc7c349dcefc5db7b3b72bad
William Bourque <willbourque02@gmail.com>
2023-04-19 22:21:49 -0300

    Premier Commit
    (Commit des imports)

---
+++ main.py
@@ -0,0 +1,15 @@
+# William Bourque 1942926
+
+import pathlib
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
+import sqlite3
+import random
+from deep_translator import GoogleTranslator
+from tkinter import messagebox
+from tkinter.messagebox import showinfo
+import os
+import sys
+from functools import partial
+from tkinter import messagebox as msg
