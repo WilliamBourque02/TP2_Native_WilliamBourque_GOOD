0f3ad9ec890c8885b7f6a1a41344439dc2e5e8f1
William Bourque <willbourque02@gmail.com>
2023-04-19 22:25:36 -0300

    Fonctions SQLRequest et SQLRequestView

--- main.py
+++ main.py
@@ -19,3 +19,42 @@ from tkinter import messagebox as msg
 path = "C:/Users/willb/Documents/GitHub/TP1_Native_WilliamBourque_1942926"
 db = "C:/Users/willb/Documents/GitHub/TP1_Native_WilliamBourque_1942926/quotes.db"
 Index = 0
+
+# Fonction pour permettre l'utilisation de fonctions SQL ainsi que l'utilisation de
+# try / except pour trouver les potentielles erreurs plus facilement
+def SQLRequest(Request):
+    connection=sqlite3.connect(db, timeout=10)
+    cursor=connection.cursor()
+    try :
+        cursor.execute(Request)
+    except Exception as e:
+        messagebox.showerror("SQL Request",e)
+        print(e)
+        print(e.args[0])
+
+    connection.commit()
+    result=cursor.fetchall()
+    connection.close()
+    return (result)
+    
+# Fonction pour permettre l'utilisation des requetes SQL qui utilisent le SQL VIEW ainsi que l'utilisation de
+# try / except pour trouver les potentielles erreurs plus facilement
+def SQLRequestView(Request):
+    connection=sqlite3.connect(db, timeout=10)
+    cursor=connection.cursor()
+
+    CreateView='CREATE VIEW IF NOT EXISTS view AS SELECT ROW_NUMBER(),* FROM Citations INNER JOIN Auteurs on Citations.AuteurID=Auteurs.AuteurID'       
+    try :
+        cursor.execute(CreateView)
+    except Exception as e:
+        messagebox.showerror("SQL Request View CREATE view",e)
+
+    try :
+        cursor.execute(Request)
+    except Exception as e:
+        messagebox.showerror("SQL Request View",e)
+        print(e)
+    connection.commit()
+    result=cursor.fetchall()
+    connection.close()
+    return result
